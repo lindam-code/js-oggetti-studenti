56ba6c358b4a68ad8cdb2bc9890d15bf62297028
lindam <linda.minotti2@gmail.com>
2020-06-24 14:53:49 +0200

    Creato un array di studenti, con stampa per ognuno del nome e cognome.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,33 @@ $(document).ready(function(){
     'eta': 34
   };
   // Stampare a schermo attraverso il for in tutte le proprietà.
-  for (var key in oggettoStudente) {
-    console.log(oggettoStudente[key]);
-  };
+  // for (var key in oggettoStudente) {
+  //   console.log(oggettoStudente[key]);
+  // };
 
+  // Creare un array di oggetti di studenti.
+  var arrayClasse = [
+    {
+      'nome': 'Linda',
+      'cognome': 'Minotti',
+      'eta': 34
+    },
+    {
+      'nome': 'Alice',
+      'cognome': 'Doro',
+      'eta': 27
+    },
+    {
+      'nome': 'Lady',
+      'cognome': 'Gaga',
+      'eta': 32
+    }
+  ];
+  // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+  for (var i = 0; i < arrayClasse.length; i++) {
+    var studente = arrayClasse[i];
+    console.log(studente.nome + ' ' + studente.cognome);
+  };
 
 
 
